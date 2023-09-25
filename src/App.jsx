fa0db159fb962a825010233049bb537c0c9855bc
hungthai <hungthai@hungthais-MacBook-Pro.local>
2023-09-25 13:04:41 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,85 @@
-import './App.css';
-import Main from './components/main';
+import { useEffect, useState } from 'react';
+
+import AppleIcon from './components/icons/apple';
+import Button from './components/buttonGroup/button';
+import FacebookIcon from './components/icons/facebook';
+import GoogleIcon from './components/icons/google';
 import Layout from './components/layout';
+import Main from './components/main';
+import MinusIcon from './components/icons/minus';
+import PlusIcon from './components/icons/plus';
+import CardList from './components/cardList';
+
+import productList from './fakeData/MOCK_DATA.json';
+
+import './App.css';
+
 
 function App() {
+  const [title] = useState('Đây là trang APP nè');
+  const [count, setCount] = useState(10);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+  setProducts(productList)
+  }, []);
+
   return (
     <div className="App">
-      {/* <Layout/> */}
-      <Layout title="Đây là trag APP nè">
+      <Layout title={title}>
+        {/* <button onClick={() => setCount(count + 10)}> Count click</button> */}
+
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            margin: '30px',
+          }}
+        >
+          <Button
+            icon={<MinusIcon />}
+            title="Trừ"
+            buttonClass="button_secondary"
+            iconClass="icon_secondary"
+            titleClass="title_secondary"
+            count={count}
+            onClick={() => setCount(count - 1)}
+          />
+
+          <Button
+            icon={<PlusIcon />}
+            title="Cộng"
+            count={count}
+            onClick={() => setCount(count + 1)}
+          />
+
+          <Button
+            title="Nhân đôi"
+            icon={<i class="fa-solid fa-xmark fa-2xl"></i>}
+            buttonClass="button_secondary"
+            iconClass="icon_secondary"
+            titleClass="title_secondary"
+            count={count}
+            onClick={() => setCount(count * 2)}
+          />
+
+          <Button icon={<AppleIcon />} title="Continue with Apple" />
+
+          <Button icon={<GoogleIcon />} title="Continue with Google" />
+
+          <Button
+            icon={<FacebookIcon />}
+            title="Continue with Facebook"
+            buttonClass="button_secondary"
+            iconClass="icon_secondary"
+            titleClass="title_secondary"
+          />
+
+          <CardList list={products} />
+        </div>
         <Main />
       </Layout>
-
     </div>
   );
 }
---
+++ src/components/card/index.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { formatter, ratingStar } from '../../helper';
+
+import './card.css';
+
+function Card(props) {
+  const {
+    id,
+    discounted,
+    image,
+    shopName,
+    newPrice,
+    oldPrice,
+    discount,
+    productName,
+    rate,
+    stock,
+    sold,
+  } = props;
+
+
+
+  return (
+    <div className="card">
+      <span className="discounted">
+        {`-${discounted}%`}
+      </span>
+
+      <div className="card-header">
+        <img className="product-image" src={image} alt="Product" />
+      </div>
+
+      <div className="card-body">
+        <h5 className="shop_name">{shopName}</h5>
+        <div className="break-line"/>
+
+        <div className="price-wrapper">
+          <p className="new_price">{formatter.format(newPrice)}</p>
+
+          <p className="old_price">{formatter.format(oldPrice)}</p>
+
+          <p className="discount">{discount}% off</p>
+        </div>
+
+        <p className="product_name">{productName}</p>
+
+        <div className="product_rate">
+          {ratingStar(rate)}
+        </div>
+
+        <progress className="product_progress" value={sold} max={stock} />
+
+        <p className="product_sold">Sold: {sold}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
