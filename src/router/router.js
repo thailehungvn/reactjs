a1ebfddc7f2f8994745f3241cfbed45979aa77aa
hungthai <hungthai@hungthais-MacBook-Pro.local>
2023-09-27 00:27:31 +0700

    update

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-// import { LOCATIONS } from 'constants/index';
+import { LOCATIONS } from 'constants/index';
+
 import ParentComponent from 'components/example/useCallback';
 import Layout from 'components/layout';
 import FormPage from 'pages/form';
@@ -12,36 +13,26 @@ import Template from 'pages/template';
 import NotFoundPage from 'pages/404';
 import CounterApp from 'pages/counterApp';
 import TodoApp from 'pages/todoApp';
-
-const LOCATIONS = {
-  HOME_PAGE: '/',
-  PLAY_LIST: 'play-list',
-  FORM: 'form',
-  TAB: 'tabs',
-  SLIDE: 'slider',
-  PRODUCTS: 'products',
-  TEMPLATE: 'template',
-  COUNTER: 'counter',
-  TODO: 'todo',
-}
+import Profile from 'pages/profile';
 
 const routers = [
   {
     path: LOCATIONS.HOME_PAGE,
+    name: "Layout",
     element: <Layout />,
     children: [
-      { isRoot: true, element: <ParentComponent /> },
-      { path: LOCATIONS.PLAY_LIST, element: <PlayList /> },
-      { path: LOCATIONS.FORM, element: <FormPage /> },
-      { path: LOCATIONS.TAB, element: <TabPage /> },
-      { path: LOCATIONS.SLIDE, element: <SliderPage /> },
-      { path: LOCATIONS.PRODUCTS, element: <ProductsPage /> 
-    },
+      { isRoot: true, name: "Parent Component", element: <ParentComponent /> },
+      { path: LOCATIONS.PLAY_LIST, name: "Play List", element: <PlayList /> },
+      { path: LOCATIONS.FORM, name: "Form Register", element: <FormPage /> },
+      { path: LOCATIONS.TAB, name: "Tab", element: <TabPage /> },
+      { path: LOCATIONS.SLIDE, name: "Slider", element: <SliderPage /> },
+      { path: LOCATIONS.PRODUCTS, name: "Product Page", element: <ProductsPage /> },
+      { path: LOCATIONS.MY_PROFILE, name: "My Profile", element: <Profile /> },
     ]
   },
-  { path: LOCATIONS.TEMPLATE, element: <Template /> },
-  { path: LOCATIONS.COUNTER, element: <CounterApp /> },
-  { path: LOCATIONS.TODO, element: <TodoApp /> },
+  { path: LOCATIONS.TEMPLATE, name: "Template", element: <Template /> },
+  { path: LOCATIONS.COUNTER, name: "Counter", element: <CounterApp /> },
+  { path: LOCATIONS.TODO, name: "Todo", element: <TodoApp /> },
   { path: "*", element: <NotFoundPage /> },
 ];
 
